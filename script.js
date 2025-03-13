bb685ea742bd585f300392d79861764cf2f94567
Admin <Admin@PCMATEO>
2025-03-13 09:39:26 -0300

    Corazones mejorados falta mejorar la alerta

--- script.js
+++ script.js
@@ -14,15 +14,33 @@ document.addEventListener("DOMContentLoaded", function() {
     if (typeof particlesJS !== "undefined") {
         particlesJS("particles-js", {
             "particles": {
-                "number": { "value": 50 },
+                "number": { "value": 100 },
                 "shape": {
                     "type": "image",
-                    "image": { "src": "heart.jpg", "width": 20, "height": 20 }
+                    "image": { "src": "heart.png", "width": 20, "height": 20 },
                 },
-                "move": { "speed": 15 }
+                "size": { "value": 30, "random": true },
+                "opacity": { "value": 0, "random": true },
+                "move": {
+                    "speed": 3,
+                    "random": true,
+                    "direction": "bottom",
+                    "out_mode": "out",
+                    "straight": false
+                },
+                "line_linked": { "enable": false } // Deshabilita las líneas entre partículas
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": { "enable": true, "mode": "repulse" }
+                },
+                "modes": {
+                    "repulse": { "distance": 100, "duration": 0.4 }
+                }
             }
         });
     } else {
         console.error("particles.js no se ha cargado correctamente.");
     }
 });
+
